ccb3de40032cc57772da5cc2beacb21d99c45e96
ruwanthiThe <ruthennakoon@gmail.com>
2025-07-06 22:12:19 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      a
+      <h1 className="text-[20px] text-blue-600">Hello</h1>
     </>
   )
 }
