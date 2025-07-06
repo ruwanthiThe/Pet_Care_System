989971dd7a8826b9aef684d8140964f43717a4fe
ruwanthiThe <ruthennakoon@gmail.com>
2025-07-07 00:04:23 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 
 import './App.css'
 
@@ -7,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-[20px] text-blue-600">Hello</h1>
+      <h1>Hello</h1>
     </>
   )
 }
---
+++ frontend/src/assets/data/reviews.js
@@ -0,0 +1,23 @@
+export const reviews = [
+  {
+    id: "01",
+    name: "Amaya Silva",
+    review: "Excellent service. My dog recovered quickly thanks to their care.",
+    rating: 5,
+    
+  },
+  {
+    id: "02",
+    name: "Kasun Perera",
+    review: "The vet was very kind and explained everything clearly. Highly recommend!",
+    rating: 4.5,
+    
+  },
+  {
+    id: "03",
+    name: "Nishini Fernando",
+    review: "Quick appointment booking and good grooming service for my cat.",
+    rating: 4.8,
+    
+  },
+];
