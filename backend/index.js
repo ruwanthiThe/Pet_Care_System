d1459502ad92b42cc8c6d32b9bd532f34f7a3c48
ruwanthiThe <ruthennakoon@gmail.com>
2025-07-09 04:36:19 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 // Sample route
 app.get('/', (req, res) => {
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-green-500 text-white text-center p-8">
+      <h1 className="text-3xl font-bold">Tailwind CSS is working!</h1>
+      <p className="mt-4">If you see a green background with white text, it's working.</p>
+    </div>
+  );
+}
+
+export default App;
